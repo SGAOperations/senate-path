ecc472a02dabfbb1b18f4f2f13a4f97688c7492f
russell-leung <russell-leung@comcast.net>
2024-03-17 16:24:24 -0400

    Fix field validation

--- apps/backend/src/applications/dto/create-application-request.dto.ts
+++ apps/backend/src/applications/dto/create-application-request.dto.ts
@@ -1,18 +1,34 @@
-import { IsEmail } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  IsPositive,
+} from 'class-validator';
 
 export class CreateApplicationRequestDto {
+  @IsString()
   fullName: string;
+  @IsString()
   preferredFullName: string;
+  @IsString()
   phoneticPronunciation: string;
+  @IsString()
   nickname: string;
+  @IsString()
   nuid: string;
+  @IsString()
   pronouns: string;
   @IsEmail()
   email: string;
+  @IsString()
   phoneNumber: string;
+  @IsPositive()
   year: number;
+  @IsString()
   college: string;
+  @IsString()
   major: string;
+  @IsString()
   minors: string;
+  @IsString()
   constituency: string;
 }
