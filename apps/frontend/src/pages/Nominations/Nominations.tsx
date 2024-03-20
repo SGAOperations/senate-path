aa9fd656323691f0ab1acfd9d2f32632d8325e88
matthewliii <matthewli4859@gmail.com>
2024-03-20 13:50:29 -0400

    All content added
    Need to style and add functionality

--- apps/frontend/src/pages/Nominations/Nominations.tsx
+++ apps/frontend/src/pages/Nominations/Nominations.tsx
@@ -1,4 +1,4 @@
-import { FormGroup, FormLabel, Box, Typography, TextField, InputLabel, Select, MenuItem, FormControl } from '@mui/material';
+import { FormGroup, FormLabel, Box, Typography, TextField, InputLabel, Select, MenuItem, FormControl, RadioGroup, FormControlLabel, Radio } from '@mui/material';
 const Nominations: React.FC = () => {
   return (
     <>
@@ -23,14 +23,12 @@ const Nominations: React.FC = () => {
           </Typography>
         </FormGroup>
         <FormControl>
-
-        </FormControl>
-        <FormGroup>
+        <FormControl required>
           <Typography variant="subtitle1">
-            What is your full name?
+          What is your full name?
           </Typography>
           <Typography variant="body1">
-            Please enter your first and last name as they appear in the official university records.
+          Please enter your first and last name as they appear in the official university records.
           </Typography>
           <TextField
           required
@@ -38,7 +36,7 @@ const Nominations: React.FC = () => {
           label="Required"
           defaultValue=""
           />
-        </FormGroup>
+        </FormControl>
         <FormGroup>
           <Typography variant="subtitle1">
             What is your Northeastern email?
@@ -53,7 +51,7 @@ const Nominations: React.FC = () => {
           defaultValue=""
           />
         </FormGroup>
-        <FormGroup>
+        <FormControl required>
           <Typography variant="subtitle1">
           Select the name of the person you are nominating.
           </Typography>
@@ -66,7 +64,93 @@ const Nominations: React.FC = () => {
             {/* Insert MenuItems using database of nominees */}
             {/* <MenuItem value={"name"}>Name</MenuItem> */}
           </Select>
-        </FormGroup>
+        </FormControl>
+        <FormControl required>
+          <Typography variant="subtitle1">
+          Please confirm you are one of the prospective senator's constituents.
+          </Typography>
+          <Typography variant="body1">
+          Select a college, organization, or program from the list below to confirm you are one of the prospective senator's constituents. <span  style={{fontWeight: 'bold'}}>You must select the same constituency as the prospective senator for this nomination to be processed.</span>
+          </Typography>
+           <InputLabel id="constituent">Constituent</InputLabel>
+            <Select
+            labelId="constituent"
+            label="Constituent"
+          >
+            {/* Insert MenuItems using database of Constituents */}
+            {/* <MenuItem value={"constituent"}>Constituent</MenuItem> */}
+          </Select>
+        </FormControl>
+
+        <FormControl required>
+          <Typography variant="subtitle1">
+          What is your college?
+          </Typography>
+          <Typography variant="body1">
+          Note: For combined majors (a single major listed in the course catalog that spans two disciplines), list only the home college. For double majors (two distinct majors listed separately in the course catalog), include both colleges.
+          </Typography>
+          <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue=""
+          />
+
+        </FormControl>
+
+        <FormControl >
+          <Typography variant="subtitle1">
+          What is your major?
+          </Typography>
+          <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue=""
+          />
+
+        </FormControl>
+
+        <FormControl required>
+          <FormGroup>
+          <Typography variant="subtitle1">
+          What is your expected graduation year?
+          </Typography>
+          <FormLabel id="demo-radio-buttons-group-label">Year</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="2023" control={<Radio />} label="2023" />
+            <FormControlLabel value="2024" control={<Radio />} label="2024" />
+            <FormControlLabel value="2025" control={<Radio />} label="2025" />
+            <FormControlLabel value="2026" control={<Radio />} label="2026" />
+            <FormControlLabel value="2027" control={<Radio />} label="2027" />
+            <FormControlLabel value="2028" control={<Radio />} label="2028" />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
+          </RadioGroup>
+          </FormGroup>
+        </FormControl>
+
+        <FormControl required>
+          <FormGroup>
+          <Typography variant="subtitle1">
+          Would you like to receive information about how to become a senator?
+          </Typography>
+          <Typography variant="body1">
+          Becoming a senator is an excellent, rewarding opportunity to serve and improve the Northeastern community.
+          </Typography>
+          <RadioGroup
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+            <FormControlLabel value="No" control={<Radio />} label="No" />
+          </RadioGroup>
+          </FormGroup>
+        </FormControl>
+
+        </FormControl>
+        
       </Box>
 
       }
