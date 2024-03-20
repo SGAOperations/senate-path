b11330caee55cd3f393b756ead344c9a21dcb93f
Natalia Ivanov <144643904+nataliaivan0v@users.noreply.github.com>
2024-03-20 15:20:13 -0400

    remove intro

--- apps/frontend/src/pages/Applications/Applications.tsx
+++ apps/frontend/src/pages/Applications/Applications.tsx
@@ -21,18 +21,6 @@ const Nominations: React.FC = () => {
       <HomeContainer>
       {
         <SampleForm>
-        <Introduction>
-          SGA Senator Application
-          <br></br>
-          Thank you for your interest in joining the Student Government Association (SGA)! SGA serves as the voice of the undergraduate student body and strives to promote student interests in the university and its surrounding communities. We have many active projects and initiatives. Read more about our work at northeasternsga.com.
-          <br></br>
-          Any undergraduate student in good academic and judicial standing is eligible to apply to become a senator. There are no elections. Read more about the process to become a senator in the frequently asked questions document (https://docs.google.com/document/d/1xDyzPBpnlzlHmPL9pd2mGsKhzQCl_Cs9EPlFb0G-Y_o/edit) and please contact Senate Speaker Donoghue at Donoghue.ca@northeastern.edu with any questions.
-          <br></br>
-          This form is the first step in becoming a senator, the second step is to gather signatures. For your application to be accepted, you need to collect at least 30 nominations from students in your constituency. If you are applying to be a special interest senator and your organization has less than 40 members, you must get three fourths of the organization’s members’ signatures. This form must be submitted so your name can be automatically added to the signature collection form. Both forms will stop accepting submissions on January 30th at 11:59 pm EST.
-          <br></br>
-          Welcome to SGA!
-          <br></br>
-        </Introduction>
 
           <FormControl>
             <FormQuestionContainer>
