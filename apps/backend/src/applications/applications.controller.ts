3f71babd978cfaea445ae179962f2f2c8adf7c9a
russell-leung <russell-leung@comcast.net>
2024-03-20 14:02:25 -0400

    Add Application Put endpoint

--- apps/backend/src/applications/applications.controller.ts
+++ apps/backend/src/applications/applications.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, Put, Param } from '@nestjs/common';
 
 import { ApplicationsService } from './applications.service';
 import { CreateApplicationRequestDto } from './dto/create-application-request.dto';
+import { UpdateApplicationRequestDto } from './dto/update-application-request.dto';
 
 @Controller('/applications')
 export class ApplicationsController {
@@ -15,6 +16,26 @@ export class ApplicationsController {
   @Post()
   createApplication(@Body() request: CreateApplicationRequestDto) {
     return this.applicationsService.createApplication({
+      fullName: request.fullName,
+      preferredFullName: request.preferredFullName,
+      phoneticPronunciation: request.phoneticPronunciation,
+      nickname: request.nickname,
+      nuid: request.nuid,
+      pronouns: request.pronouns,
+      email: request.email,
+      phoneNumber: request.phoneNumber,
+      year: request.year,
+      college: request.college,
+      major: request.major,
+      minors: request.minors,
+      constituency: request.constituency,
+    });
+  }
+
+  @Put('/:id')
+  updateApplication(@Param('id') id: number, @Body() request: UpdateApplicationRequestDto) {
+    return this.applicationsService.updateApplication({
+      id: id,
       fullName: request.fullName,
       preferredFullName: request.preferredFullName,
       phoneticPronunciation: request.phoneticPronunciation,
--- apps/backend/src/applications/applications.service.ts
+++ apps/backend/src/applications/applications.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 
 import supabase from '../supabase/client';
 import { Tables } from '../supabase/database.types';
@@ -59,6 +59,69 @@ export class ApplicationsService {
       minors,
       constituency,
     });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async updateApplication({
+    id,
+    fullName,
+    preferredFullName,
+    phoneticPronunciation,
+    nickname,
+    nuid,
+    pronouns,
+    email,
+    phoneNumber,
+    year,
+    college,
+    major,
+    minors,
+    constituency,
+  }: {
+    id: number;
+    fullName: string;
+    preferredFullName: string;
+    phoneticPronunciation: string;
+    nickname: string;
+    nuid: string; 
+    pronouns: string;
+    email: string;
+    phoneNumber: string;
+    year: number;
+    college: string;
+    major: string;
+    minors: string;
+    constituency: string;
+  }): Promise<void> {
+
+    const { data: applicationData, error: applicationError } = await supabase.from('applications').select('*').eq('id', id);
+    if (applicationData.length === 0) {
+      throw new HttpException({
+        status: HttpStatus.BAD_REQUEST,
+        error: `Application with given id, ${id}, does not exist.`,
+      }, HttpStatus.BAD_REQUEST, {
+        cause: applicationError
+      });
+    }
+
+    const { error } = await supabase.from('applications').update({
+      fullName,
+      preferredFullName,
+      phoneticPronunciation,
+      nickname,
+      nuid,
+      pronouns,
+      email,
+      phoneNumber,
+      year,
+      college,
+      major,
+      minors,
+      constituency,
+    }).eq('id', id);
 
     if (error) {
       throw new Error(error.message);
