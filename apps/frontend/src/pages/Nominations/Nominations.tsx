1297760b4e2fe3f5441e2522f5c88fb7ebdeee07
matthewliii <matthewli4859@gmail.com>
2024-03-25 01:22:19 -0400

    Styled components added

--- apps/frontend/src/pages/Nominations/Nominations.tsx
+++ apps/frontend/src/pages/Nominations/Nominations.tsx
@@ -1,4 +1,5 @@
 import { FormGroup, FormLabel, Box, Typography, TextField, InputLabel, Select, MenuItem, FormControl, RadioGroup, FormControlLabel, Radio, Button } from '@mui/material';
+import { HomeContainer, FormInput, SampleForm, FormTextContainer, FormQuestionContainer, FormTextAnswerContainer, Introduction, FormInputCheckbox, FormSelect, RadioButtons } from '../Applications/styles';
 import React, { useState } from 'react';
 
 const Nominations: React.FC = () => {
@@ -29,19 +30,24 @@ const Nominations: React.FC = () => {
   return (
     <>
       {/* TODO delete the above text and replace it with the nominations form here */
-      <Box>
-        <FormGroup>
-            <h1>SGA Senator Nomination Form</h1>
+      <HomeContainer>
+        <SampleForm>
+            <h1 style={{textAlign: 'center' }}>SGA Senator Nomination Form</h1>
             <p>Complete this form to nominate a person to become a senator in the Student Government Association (SGA). SGA serves as the voice of the undergraduate student body and strives to promote student interests in the university and its surrounding communities. To learn more about SGA, visit our website at northeasternsga.com.</p>
             <p>This form is a nomination, not a vote. It is simply a statement you would like to see one of your peers become a senator in SGA. You may complete this form for an unlimited number of prospective senators, but you may only nominate each student once. You must belong to the same constituency as the prospective senator seeks to represent (so only undergraduate students in the College of Engineering may nominate senators for the College of Engineering, only NUin students may nominate senators for the NUin program, etc).</p>
             <p>SGA senator applications are currently open. To apply for a senatorship, visit <a href="">Senator Applications</a></p>
             <p>Please contact Cassidy Donoghue at donoghue.ca@northeastern.edu with any questions.</p>
-        </FormGroup>
-        <FormControl>
-        <FormControl required>
-          <h1>What is your full name?</h1>
-          <p>Please enter your first and last name as they appear in the official university records.</p>
-          <TextField
+        </SampleForm>
+
+          <SampleForm>
+          <FormControl required>
+          <FormQuestionContainer>
+            <FormTextContainer>
+            <h1>What is your full name?</h1>
+            <p>Please enter your first and last name as they appear in the official university records.</p>
+            </FormTextContainer>
+            <FormTextAnswerContainer>
+            <TextField
           required
           id="outlined-required"
           label="Required"
@@ -50,25 +56,42 @@ const Nominations: React.FC = () => {
             setFullName(e.target.value)
           }
           />
-        </FormControl>
-        <FormGroup>
-          <h1>What is your Northeastern email?</h1>
-          <p>We may contact you to verify the authenticity of this nomination.</p>
-          <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue=""
-          onChange={(e) =>
-            setNortheasternEmail(e.target.value)
-          }
-          />
-        </FormGroup>
-        <FormControl required>
-          <h1>Select the name of the person you are nominating</h1>
-           <InputLabel id="nominee">Nominee</InputLabel>
-            <Select
-            labelId="nominee"
+            </FormTextAnswerContainer>
+          </FormQuestionContainer>
+          
+          
+          </FormControl>
+          </SampleForm>
+        
+        <SampleForm>
+          <FormQuestionContainer>
+            <FormTextContainer>
+            <h1>What is your Northeastern email?</h1>
+            <p>We may contact you to verify the authenticity of this nomination.</p>
+            </FormTextContainer>
+            <FormTextAnswerContainer>
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              defaultValue=""
+              onChange={(e) =>
+                setNortheasternEmail(e.target.value)
+              }
+              />
+            </FormTextAnswerContainer>
+          </FormQuestionContainer>
+        </SampleForm>
+
+        <SampleForm>
+          <FormQuestionContainer>
+            <FormTextContainer>
+            <h1>Select the name of the person you are nominating</h1>
+            </FormTextContainer>
+            <FormTextAnswerContainer>
+              
+            <FormSelect
+            required
             label="nominee"
             onChange={(e) =>
               setNominee(e.target.value as string)
@@ -76,28 +99,39 @@ const Nominations: React.FC = () => {
           >
             {/* Insert MenuItems using database of nominees */}
             <MenuItem value={"name"}>Name</MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl required>
-          <h1>Please confirm you are one of the prospective senator's constituents.</h1>
+          </FormSelect>
+            </FormTextAnswerContainer>
+          </FormQuestionContainer>
+        </SampleForm>
+
+        <SampleForm>
+          <FormQuestionContainer>
+            <FormTextContainer>
+            <h1>Please confirm you are one of the prospective senator's constituents.</h1>
           <p>Select a college, organization, or program from the list below to confirm you are one of the prospective senator's constituents. <span  style={{fontWeight: 'bold'}}>You must select the same constituency as the prospective senator for this nomination to be processed.</span></p>
-           <InputLabel id="constituent">Constituent</InputLabel>
-            <Select
-            labelId="constituent"
-            label="Constituent"
+            </FormTextContainer>
+            <FormTextAnswerContainer>
+            <FormSelect
+            required
             onChange={(e) =>
               setConstituent(e.target.value as string)
             }
           >
             {/* Insert MenuItems using database of Constituents */}
             <MenuItem value={"constituent"}>Constituent</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl required>
-          <h1>What is your college?</h1>
+          </FormSelect>
+            </FormTextAnswerContainer>
+          </FormQuestionContainer>
+        </SampleForm>
+          
+        <SampleForm>
+          <FormQuestionContainer>
+            <FormTextContainer>
+            <h1>What is your college?</h1>
           <p>Note: For combined majors (a single major listed in the course catalog that spans two disciplines), list only the home college. For double majors (two distinct majors listed separately in the course catalog), include both colleges.</p>
-          <TextField
+            </FormTextContainer>
+            <FormTextAnswerContainer>
+            <TextField
           required
           id="outlined-required"
           label="Required"
@@ -106,12 +140,17 @@ const Nominations: React.FC = () => {
             setCollege(e.target.value)
           }
           />
+            </FormTextAnswerContainer>
+          </FormQuestionContainer>
+        </SampleForm>
 
-        </FormControl>
-
-        <FormControl >
-          <h1>What is your major?</h1>
-          <TextField
+          <SampleForm>
+            <FormQuestionContainer>
+              <FormTextContainer>
+              <h1>What is your major?</h1>
+              </FormTextContainer>
+              <FormTextAnswerContainer>
+              <TextField
           required
           id="outlined-required"
           label="Required"
@@ -120,16 +159,19 @@ const Nominations: React.FC = () => {
             setMajor(e.target.value)
           }
           />
+              </FormTextAnswerContainer>
+            </FormQuestionContainer>
+          </SampleForm>
 
-        </FormControl>
-
-        <FormControl required>
-          <FormGroup>
-            <h1>What is your expected graduation year?</h1>
-          <FormLabel id="demo-radio-buttons-group-label">Year</FormLabel>
+          <SampleForm>
+            <FormQuestionContainer>
+              <FormTextContainer>
+                <h1>What is your expected graduation year?</h1>
+              </FormTextContainer>
+              <RadioButtons>
           <RadioGroup
-            aria-labelledby="demo-radio-buttons-group-label"
-            name="radio-buttons-group"
+            name="year-buttons-group"
+            aria-required
             onChange={(e) =>
               setGradYear(e.target.value)
             }
@@ -142,15 +184,20 @@ const Nominations: React.FC = () => {
             <FormControlLabel value="2028" control={<Radio />} label="2028" />
             <FormControlLabel value="other" control={<Radio />} label="Other" />
           </RadioGroup>
-          </FormGroup>
-        </FormControl>
+              </RadioButtons>
+            </FormQuestionContainer>
+          </SampleForm>
 
-        <FormControl required>
-          <FormGroup>
+          <SampleForm>
+            <FormQuestionContainer>
+            <FormTextContainer>
             <h1>Would you like to receive information about how to become a senator?</h1>
             <p>Becoming a senator is an excellent, rewarding opportunity to serve and improve the Northeastern community.</p>
-          <RadioGroup
-            name="radio-buttons-group"
+            </FormTextContainer>
+            <RadioButtons>
+            <RadioGroup
+            aria-required
+            name="receive-buttons-group"
             onChange={(e) =>
               setReceiveInfo(e.target.value)
             }
@@ -158,15 +205,17 @@ const Nominations: React.FC = () => {
             <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
             <FormControlLabel value="No" control={<Radio />} label="No" />
           </RadioGroup>
-          </FormGroup>
-        </FormControl>
+            </RadioButtons>
+            </FormQuestionContainer>
+            
+          </SampleForm>
+
 
         <Button variant="contained" onClick={handleSampleFormSubmit}>
             Submit
           </Button>
-        </FormControl>
         
-      </Box>
+      </HomeContainer>
 
       }
     </>
