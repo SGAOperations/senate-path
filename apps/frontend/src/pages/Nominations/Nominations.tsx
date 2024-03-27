76262e4b0585a5f2975fcc38d902691d4cce50f8
Kenny Jung <dukyoung01@naver.com>
2024-03-27 13:15:47 -0400

    Fix cors + fetch example

--- apps/frontend/src/pages/Nominations/Nominations.tsx
+++ apps/frontend/src/pages/Nominations/Nominations.tsx
@@ -1,4 +1,23 @@
+import { useEffect } from 'react';
+
 const Nominations: React.FC = () => {
+  const fetchData = () => {
+    fetch('http://localhost:3000/api/applications')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       I am the nominations page
