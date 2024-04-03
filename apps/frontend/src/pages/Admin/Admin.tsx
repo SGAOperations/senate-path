98b08580abcd525c7e103ff7a4ea67e82ba74010
Jake Wu-Chen <jwcgolf12@gmail.com>
2024-04-03 14:55:41 -0400

    fixed errors

--- apps/frontend/src/pages/Admin/Admin.tsx
+++ apps/frontend/src/pages/Admin/Admin.tsx
@@ -7,16 +7,16 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-interface Nomination {
+interface Entry {
   id: string;
   fullName: string;
   email: string;
 }
 
 const Admin: React.FC = () => {
-  const [nominations, setNominations] = useState<Nomination[]>([]);
-  
-  useEffect(() => {
+  const [nominations, setNominations] = useState<Entry[]>([]);
+  const [applications, setApplications] = useState<Entry[]>([]);
+  const getNominations = () => {
     fetch('http://localhost:3000/api/nominations', {
       method: 'GET',
     })
@@ -32,7 +32,26 @@ const Admin: React.FC = () => {
     .catch(error => {
       console.error('Error fetching nominations:', error);
     });
-  }, []);
+  } 
+  const getApplications = () => {
+    fetch('http://localhost:3000/api/applications', {
+      method: 'GET',
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch applications');
+      }
+      return response.json();
+    })
+    .then(data => {
+      setApplications(data);
+    })
+    .catch(error => {
+      console.error('Error fetching applications:', error);
+    });
+  } 
+  useEffect(getNominations, []);
+  useEffect(getApplications, []);
   
   return (
     <div>
@@ -68,11 +87,11 @@ const Admin: React.FC = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {applications.map(applications => (
-              <TableRow key={applications.id}>
-                <TableCell>{applications.id}</TableCell>
-                <TableCell>{applications.fullName}</TableCell>
-                <TableCell>{applications.email}</TableCell>
+            {applications.map(application => (
+              <TableRow key={application.id}>
+                <TableCell>{application.id}</TableCell>
+                <TableCell>{application.fullName}</TableCell>
+                <TableCell>{application.email}</TableCell>
               </TableRow>
             ))}
           </TableBody>
