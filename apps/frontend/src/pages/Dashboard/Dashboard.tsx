e97ea55b3f82f3a70a51590b9db5302dfa54c3b5
matthewliii <matthewli4859@gmail.com>
2024-04-10 13:30:30 -0400

    Congratulatory message implemented

--- apps/frontend/src/pages/Dashboard/Dashboard.tsx
+++ apps/frontend/src/pages/Dashboard/Dashboard.tsx
@@ -4,8 +4,6 @@ import { ResultType } from '@remix-run/router/dist/utils';
 
 const Dashboard: React.FC = () => {
   const [numOfNoms, setNumOfNoms] = useState(0);
-  const setNums = import.meta.env.VITE_NUM_MIN_NOMINATIONS;
-  const neededNoms = setNums - numOfNoms;
   const receiveNominations = () => {
     fetch('http://localhost:3000/api/nominations', {
       method: 'GET',
@@ -21,18 +19,32 @@ const Dashboard: React.FC = () => {
   };
 
   receiveNominations();
-  return (
-    <HomeContainer>
-      <Nominations>
-        <h1>Nominations</h1>
-        <h3>{numOfNoms}</h3>
-      </Nominations>
-      <Nominations>
-        <h1>Nominations Needed</h1>
-        <h3>{neededNoms}</h3>
-      </Nominations>
-    </HomeContainer>
-  );
+
+  const setNums = import.meta.env.VITE_NUM_MIN_NOMINATIONS;
+
+  const neededNoms = setNums - numOfNoms;
+  if (neededNoms <= 0) {
+    return (
+      <HomeContainer>
+        <h1>
+          Congratulations! You have met the required amount of nominations.
+        </h1>
+      </HomeContainer>
+    );
+  } else {
+    return (
+      <HomeContainer>
+        <Nominations>
+          <h1>Nominations</h1>
+          <h3>{numOfNoms}</h3>
+        </Nominations>
+        <Nominations>
+          <h1>Nominations Needed</h1>
+          <h3>{neededNoms}</h3>
+        </Nominations>
+      </HomeContainer>
+    );
+  }
 };
 
 export default Dashboard;
