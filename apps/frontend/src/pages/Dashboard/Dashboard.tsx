c28a7ada4ea49485e85522d1e618d2fe8feeab74
matthewliii <matthewli4859@gmail.com>
2024-04-10 13:14:57 -0400

    Environment variables implemented

--- apps/frontend/src/pages/Dashboard/Dashboard.tsx
+++ apps/frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,40 +1,38 @@
-import { HomeContainer, Nominations } from "./styles";
+import { HomeContainer, Nominations } from './styles';
 import React, { useState } from 'react';
-import { ResultType } from "@remix-run/router/dist/utils";
+import { ResultType } from '@remix-run/router/dist/utils';
 
 const Dashboard: React.FC = () => {
-
   const [numOfNoms, setNumOfNoms] = useState(0);
-  const neededNoms = 100 - numOfNoms;
+  const setNums = import.meta.env.VITE_NUM_MIN_NOMINATIONS;
+  const neededNoms = setNums - numOfNoms;
   const receiveNominations = () => {
     fetch('http://localhost:3000/api/nominations', {
-      method : "GET"
+      method: 'GET',
     })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result.length);
-      setNumOfNoms(result.length);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-    
-  }
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result.length);
+        setNumOfNoms(result.length);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   receiveNominations();
   return (
     <HomeContainer>
       <Nominations>
-      <h1>Nominations</h1>
-      <h3>{numOfNoms}</h3>
-    </Nominations>
-    <Nominations>
-      <h1>Nominations Needed</h1>
-      <h3>{neededNoms}</h3>
-    </Nominations>
+        <h1>Nominations</h1>
+        <h3>{numOfNoms}</h3>
+      </Nominations>
+      <Nominations>
+        <h1>Nominations Needed</h1>
+        <h3>{neededNoms}</h3>
+      </Nominations>
     </HomeContainer>
-    
-  ) 
-  ;
+  );
 };
 
 export default Dashboard;
