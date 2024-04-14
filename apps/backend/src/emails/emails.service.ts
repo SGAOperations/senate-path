65a99d3cbbca135f43992c05c1a24627620ada90
russell-leung <russell-leung@comcast.net>
2024-04-13 21:39:50 -0400

    Email endpoint update using node mailer

--- apps/backend/src/emails/emails.service.ts
+++ apps/backend/src/emails/emails.service.ts
@@ -1,30 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 import { CreateEmailRequestDto } from './dto/create-email-request.dto';
 
 @Injectable()
 export class EmailsService {
-  async createEmail(request: CreateEmailRequestDto): Promise<Response> {
-    const res = await fetch('https://api.resend.com/emails', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
-      },
-      body: JSON.stringify({
-        from: process.env.RESEND_FROM_EMAIL,
-        to: process.env.RESEND_TO_EMAIL,
-        bcc: request.recipients,
-        subject: request.subject,
-        text: request.message,
-      }),
-    });
+  constructor(private mailerService: MailerService) {}
 
-    const data = await res.json();
-
-    return new Response(JSON.stringify(data), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
+  async createEmail(request: CreateEmailRequestDto): Promise<void> {
+    await this.mailerService.sendMail({
+      to: process.env.EMAIL_USER,
+      from: process.env.EMAIL_FROM,
+      bcc: request.recipients,
+      subject: request.subject,
+      template: './email-template',
+      context: {
+        message: request.message,
       },
     });
   }
