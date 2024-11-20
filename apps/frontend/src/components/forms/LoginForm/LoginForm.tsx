ea6261b74d2335d116d879c454d6f7ba8beb0102
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2024-11-20 00:09:53 -0500

    NSH-12 lay out full form with submit button

--- apps/frontend/src/components/forms/LoginForm/LoginForm.tsx
+++ apps/frontend/src/components/forms/LoginForm/LoginForm.tsx
@@ -1,16 +1,39 @@
-import { FormControl, TextField } from '@mui/material';
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  Input,
+  InputLabel,
+  TextField,
+} from '@mui/material';
+import { useState } from 'react';
 
 interface Props {
   setLoginStatus: (isLoggedIn: boolean) => void;
 }
 
 export const LoginForm: React.FC<Props> = ({ setLoginStatus }) => {
+  const username = useState<string>('');
+  const password = useState<string>('');
+
+  const onSubmit = () => {};
+
   return (
     <>
       <FormControl>
-        <p>Password</p>
         <TextField
-          label="Required"
+          label="Username"
+          required
+          placeholder="Username"
+          onChange={(e) =>
+            e.target.value == 'password' ? setLoginStatus(true) : null
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <TextField
+          type="password"
+          label="Password"
           required
           placeholder="Password"
           onChange={(e) =>
@@ -18,6 +41,9 @@ export const LoginForm: React.FC<Props> = ({ setLoginStatus }) => {
           }
         />
       </FormControl>
+      <Button variant="contained" onClick={onSubmit}>
+        Submit
+      </Button>
     </>
   );
 };
