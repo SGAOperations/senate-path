06c83ec936534376dd19777095a957f63b4b6058
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-10-31 20:32:55 -0400

    create init migration

---
+++ prisma/migrations/20251101003228_init/migration.sql
@@ -0,0 +1,54 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'APPLICANT', 'STANDARD');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'STANDARD',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "applications" (
+    "id" SERIAL NOT NULL,
+    "nuid" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "preferredFullName" TEXT NOT NULL,
+    "nickname" TEXT NOT NULL,
+    "phoneticPronunciation" TEXT NOT NULL,
+    "pronouns" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "college" TEXT NOT NULL,
+    "major" TEXT NOT NULL,
+    "minors" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "semester" TEXT NOT NULL DEFAULT '',
+    "constituency" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "applications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "nominations" (
+    "id" SERIAL NOT NULL,
+    "nominee" TEXT,
+    "fullName" TEXT,
+    "email" TEXT,
+    "college" TEXT,
+    "major" TEXT,
+    "graduationYear" INTEGER,
+    "constituency" TEXT,
+    "receiveSenatorInfo" BOOLEAN DEFAULT false,
+    "status" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "nominations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "applications_nuid_key" ON "applications"("nuid");
