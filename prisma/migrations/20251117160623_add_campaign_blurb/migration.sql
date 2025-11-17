fa7b583e63d6bfd2e3f5a8174e624c9886bfb53d
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-11-17 16:29:34 -0500

    #95 make campaign blurb empty by default

--- prisma/migrations/20251117160623_add_campaign_blurb/migration.sql
+++ prisma/migrations/20251117160623_add_campaign_blurb/migration.sql
@@ -1,8 +1,2 @@
-/*
-  Warnings:
-
-  - Added the required column `campaignBlurb` to the `applications` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "applications" ADD COLUMN     "campaignBlurb" TEXT NOT NULL;
+ALTER TABLE "applications" ADD COLUMN     "campaignBlurb" TEXT NOT NULL DEFAULT '';
